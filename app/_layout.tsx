069178d9bba7187908f015b143fe6577af1d713f
yaredow <yaredyilma11@gmail.com>
2026-05-13 18:34:15 +0300

    feat: reset the project and configure tanstack-query with online state managemnet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from "expo-router";
+import { QueryProvider } from "@/lib/query-provider";
+import { useOnlineManager } from "@/hooks/use-online-manager";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  useOnlineManager();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <QueryProvider>
+      <Stack />
+    </QueryProvider>
   );
 }
